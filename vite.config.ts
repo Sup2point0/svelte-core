7ddd5ac935fee0bac736977cbd6ebc8bf09afa29
Sup#2.0 <sup2point0@gmail.com>
2026-07-08 20:36:50 +0100

    default to Edge browser on `vite dev`

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
+
+// yes, I prefer Edge's smooth scrolling :P
+if (process.argv.includes("--open")) {
+  process.env.BROWSER = "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe";
+}
+
+
 export default defineConfig({
   plugins: [sveltekit()]
 });
